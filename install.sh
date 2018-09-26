15ff254928387a14a00406066900d1535b413fb0
Damon Oehlman <damon.oehlman@gmail.com>
2018-09-26 17:40:43 +1000

    create installation functions, machine prep working

--- install.sh
+++ install.sh
@@ -1,41 +1,99 @@
 #!/usr/bin/env bash
 
-# to create the partitions programatically (rather than manually)
-# we're going to simulate the manual input to fdisk
-# The sed script strips off all the comments so that we can 
-# document what we're doing in-line with the actual commands
-# Note that a blank line (commented as "defualt" will send a empty
-# line terminated with a newline to take the fdisk default.
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+set -eu
+
+NIXOS_BASE="/mnt/etc/nixos"
+
+partition() {
+  local target_device=$1
+
+  sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "${target_device}"
   g # gpt partition table
   w # write and exit
 EOF
 
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "${target_device}"
   n # new partition
   p # primary partition
   1 # partition number 1
     # default - start at beginning of disk 
-  +100M # 100 MB boot parttion
+  +100M
   n # new partition
   e # extended partition
   3 # partition 3
     # default - next starting block
-  +2G # 2G swap partition
+  +512M
   t # change partition type
   3 # partition 3
-  82 # linux swap
+  19 # linux swap (not 82)
   n # new partition
   p # primary partition
   2 # partion number 2
     # default, start immediately after preceding partition
     # default, extend partition to end of disk
-  a # make a partition bootable
+  x # expert options
+  A # legacy bootable flag
   1 # bootable partition is partition 1 -- /dev/sda1
+  r # return to main menu
   p # print the in-memory partition table
   w # write the partition table
 EOF
+}
 
-# format the boot partition
-mkfs.ext4 -L nixos /dev/sda1
+prepare_disks() {
+  local target_device=$1
 
+  partition "${target_device}"
+  mkfs.ext4 -L nixos "${target_device}1"
+  mkswap "${target_device}3"
+
+  mount "${target_device}1" /mnt
+  swapon "${target_device}3"
+}
+
+generate_config() {
+  local target_device=$1
+  local nix_config
+
+  echo "generating base config"
+  nixos-generate-config --force --root /mnt
+
+  echo "generating custom config"
+  read -r -d '' nix_config <<EOF || true
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.grub.device = "${target_device}";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+
+  system.stateVersion = "18.03";
+}
+EOF
+
+  mv -f "${NIXOS_BASE}"/configuration.nix "${NIXOS_BASE}"/generated-configuration.nix
+  echo "${nix_config}" > /mnt/etc/nixos/configuration.nix
+}
+
+reset_machine() {
+  local target_device=$1
+
+  umount /mnt 2>/dev/null || true
+  swapoff "${target_device}3" 2>/dev/null || true
+}
+
+main() {
+  local target_device=$1
+
+  reset_machine "${target_device}"
+  prepare_disks "${target_device}"
+
+  generate_config "${target_device}"
+  #nixos-install
+}
+
+main "/dev/sda"
